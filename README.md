40e7eee773fd018e31495f5cc5b8448fc705610c
“Emilio” <emilio.jeldes@gmail.com>
2018-07-21 16:21:27 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# [React Native Udemy Course](https://www.udemy.com/react-native-the-practical-guide/)
+
+## This repo contains all my progress on the course using directories for each unit.
+
+- [hello-world](http://link)
+- [basics-componets](http://link)
---
+++ hello-world/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>Hello World!!!</Text>
+        <Text>First React Native App</Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center'
+  }
+});
